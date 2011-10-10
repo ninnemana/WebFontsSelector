945e4ef46fb2c4ea5a588829ef787b444b2cbfc4
Alex Ninneman <ninnemana@gmail.com>
2011-10-10 01:00:31 +0000

    pushing display with selectors of all Google Web Fonts, now to add filtering and paging

--- js/script.js
+++ js/script.js
@@ -8,25 +8,30 @@ var preview_text = 'Grumpy wizards make toxic brew for the evil Queen and Jack.'
 
 
 $(function(){
-    
+        
     $.get(fonts_api_domain + key +'&callback=getFonts', {'sort':'popularity'},function(){},'jsonp');
     
     });
     
     function getFonts(data){
         var container = $('div[role=main]');
+        var html = '';
         $.each(data.items,function(i,font){
             var link = document.createElement('link');
             link.rel = 'stylesheet';
             link.href = 'http://fonts.googleapis.com/css?family='+font.family+'&text='+preview_text;
             $('head').append(link);
             
-            var html = '<div class="font-listing">';
+            html += '<div class="font-listing">';
             html += '<div class="preview-select">';
+            html += '<div class="select">';
             html += '<input type="radio" name="select-font" id="'+font.family+'" value="'+font.famiy+'" />';
-            html += '<p contenteditable>'+preview_text+'</p>';
+            html += '</div>';
+            html += '<input type="text" style="font-family:'+font.family+'" value="'+preview_text+'"/>';
+            html += '<div style="clear:both"></div>';
             html += '</div>';
             html += '</div>';
-            $(container).append(html);
             });
+            $(container).append(html);
+            $('#loading').hide();
     }
